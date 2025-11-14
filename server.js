bc890264891e6bfd7df655b5ba33f6b30878ff54
Ananya Jain <78138175+ananyajain10@users.noreply.github.com>
2025-11-14 22:05:45 +0530

    added lh

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 const allowedOrigins = [
   "https://prompt-paraphrase-pro.vercel.app",
-  "http://localhost:8080",
-  "http://localhost:8000"
 ];
 
 app.use(cors({
